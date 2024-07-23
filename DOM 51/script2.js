adff3321663ea3e4bb90a21d48d4a358a070e206
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2024-07-23 23:42:33 +0500

    DOM 51,52 and 53 are pushed

---
+++ DOM 51/script2.js
@@ -0,0 +1,56 @@
+// const allImages = document.images;
+// const allImages = document.getElementsByTagName("img");
+// const cssImage = document.getElementsByClassName("css-image")[0];
+// const css2Image = document.getElementById("css-image");
+// // css2Image.src =""
+
+// const hello = document.querySelector(".css-image");
+
+// const li = document.querySelector("ul li");
+// function sayHi() {
+//   hello.src =
+//     "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8=";
+// }
+// function heya(params) {
+//   const jsImage = document.querySelector("#js-image");
+//   jsImage.src =
+//     "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1721520000&semt=sph";
+// }
+
+// const imagesURL = [
+//   "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1721520000&semt=sph",
+//   "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8=",
+//   "https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?cs=srgb&dl=pexels-8moments-1266810.jpg&fm=jpg",
+// ];
+// //js easiness
+// const ul = document.querySelector("ul");
+// const cssImage = ul.querySelector(".css-image");
+
+// // const allImages = document.querySelectorAll('img')
+
+// const allImages = [...document.images];
+// function updaterOfAllImages() {
+//   for (let i = 0; i < allImages.length; i++) {
+//     allImages[i].src = imagesURL[i];
+//   }
+// }
+
+// allImages.forEach(function (images, index) {
+//   images.src = imagesURL[index];
+// });
+
+// const fruits = ["apple", "banana", "mango"];
+
+// fruits.forEach(function (fruit, index) {
+//   console.log(index + ": " + fruit);
+// });
+
+
+
+let hope =["abc","def","ghi"];
+
+hope.forEach((name,index)=>
+{
+    console.log(`${name} is at index ${index}`);
+}
+)
---
+++ DOM 52/script.js
@@ -0,0 +1,6 @@
+const p = document.querySelector('p');
+console.log(p.innerHTML);
+
+
+console.log(p.innerText);
+console.log(p.textContent);
